9fcfc527b6a3c1f6bdc5a4ddf2f49bab7329a14f
George Eisa <george.eisa7@gmail.com>
2016-11-20 08:23:31 -0500

    Working Facebook Login

--- Pronto/app/src/main/java/com/redeyesoftware/pronto/MainPage.java
+++ Pronto/app/src/main/java/com/redeyesoftware/pronto/MainPage.java
@@ -1,17 +1,9 @@
 package com.redeyesoftware.pronto;
 
-import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class MainPage extends AppCompatActivity {
 
@@ -19,22 +11,6 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
-
-        // Add code to print out the key hash
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo(
-                    "com.redeyesoftware.pronto",
-                    PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-            Log.d("KeyHash:", "1");
-        } catch (NoSuchAlgorithmException e) {
-            Log.d("KeyHash:", "2");
-        }
 
         HomePagesAdapter mFragPagerAdapter = new HomePagesAdapter(getSupportFragmentManager());
         ViewPager mViewPager = (ViewPager) findViewById(R.id.pager);
